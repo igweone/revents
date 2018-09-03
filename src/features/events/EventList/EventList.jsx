42d415aaa11fa776a42c9f20404f04cc2011c92c
igweone <emeosigwe@gmail.com>
2018-09-03 16:14:36 +0100

    added dummy data to static page

--- src/features/events/EventList/EventList.jsx
+++ src/features/events/EventList/EventList.jsx
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import EventListItem from "./EventListItem";
 class EventList extends Component {
   render() {
+    const { events } = this.props;
     return (
       <div>
         <h1> EventList</h1>
-        <EventListItem />
-        <EventListItem />
-        <EventListItem />
+        {events.map(event => (
+          <EventListItem key={event.id} event={event} />
+        ))}
       </div>
     );
   }
